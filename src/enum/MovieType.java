fb1d163e6d7bdd4410efc639f854b72f3f538a6e
Hakan Eröztekin <h.eroztekin@gmail.com>
2020-04-05 08:26:44 +0300

    init small changes

---
+++ src/enum/MovieType.java
@@ -0,0 +1,13 @@
+public enum MovieType {
+    REGULAR(0),
+    NEW_RELEASE(1),
+    CHILDRENS(2);
+    
+    @Getter
+    private int value;
+    
+    public MovieType(int value) {
+        this.value = value;
+    }
+
+}
