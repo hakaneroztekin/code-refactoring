1193f031c78c1d0eb84ed13e26955301b936aa65
Hakan Eröztekin <h.eroztekin@gmail.com>
2020-04-05 09:00:06 +0300

    prepare for strategy

---
+++ src/model/Statement.java
@@ -0,0 +1,11 @@
+public class Statement {
+    
+    private double totalAmount;
+    
+    private int frequentRenterPoints;
+    
+    public Statement() {
+        totalAmount = 0;
+        frequentRenterPoints = 0;
+    }
+}
