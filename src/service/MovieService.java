508b9e11d3edce0c16189c063bcd8ab460b528e1
Hakan Eröztekin <h.eroztekin@gmail.com>
2020-04-05 09:51:10 +0300

    add helper methods

--- src/service/MovieService.java
+++ src/service/MovieService.java
@@ -3,7 +3,7 @@ public class MovieService {
       public String statement(Customer customer) {  
         Statement statement = new Statement();
             
-        String summary = "Rental Record for " + customer.getName() + "\n";
+        String summary = buildCustomerSummary();
           
         for (Rental rental : customer.getRentals()) {
             
@@ -19,13 +19,21 @@ public class MovieService {
             summary += buildRentalSummary(rental, statement);
 
         }         //add footer lines
-        summary += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        summary += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        summary += buildFooterSummary();
         return summary;
+    }
+    
+    private String buildCustomerSummary(Customer customer, Statement statement) {
+         return "Rental Record for " + customer.getName() + "\n";
     }
     
     private String buildRentalSummary(Rental rental, Statement statement) {
         return "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(statement.getTotalAmount()) + "\n";
+    }
+    
+    private String buildFooterSummary(Statement statement) {
+        return "Amount owed is " + String.valueOf(statement.getTotalAmount()) 
+            + "\n You earned " + String.valueOf(statement.getFrequentRenterPoints()) + " frequent renter points";
     }
 }
 
